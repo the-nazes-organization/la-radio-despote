e5a83bfbf883467200cc5ade980218a359ce9838
Maxime Organi <maxime.organi@gmail.com>
2024-02-17 13:56:28 +0100

    adding schema + import.meta

--- apps/client/src/lib/providers/ConvexProvider.tsx
+++ apps/client/src/lib/providers/ConvexProvider.tsx
@@ -2,7 +2,7 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 
-const convex = new ConvexReactClient(process.env.CONVEX_URL!);
+const convex = new ConvexReactClient(import.meta.env.CONVEX_URL!);
 
 export default function ConvexClientProvider({
 	children,
