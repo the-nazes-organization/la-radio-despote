0fe1b951cbdcb4fbbda2d886508d09245c4956e3
benjaminwaterlot <waterlotbenjamin@gmail.com>
2024-02-19 22:33:15 +0100

    .

---
+++ client/src/components/user-dropdown.tsx
@@ -0,0 +1,88 @@
+import { Button } from '@/components/ui/button';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuPortal,
+	DropdownMenuSeparator,
+	DropdownMenuSub,
+	DropdownMenuSubContent,
+	DropdownMenuSubTrigger,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useSpotifyPlayerStore } from '@/lib/providers/SpotifyPlayerProvider';
+import { useTheme } from '@/lib/providers/ThemeProvider';
+import { useNavigate } from '@tanstack/react-router';
+import { Check, LogOut, Moon, Settings, Sun, Users } from 'lucide-react';
+
+export function UserDropdown() {
+	const { theme, setTheme } = useTheme();
+	const spotify = useSpotifyPlayerStore();
+	const navigate = useNavigate();
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="outline" size="icon" className="rounded-full">
+					<Settings className="h-[1.2rem] w-[1.2rem]" />
+				</Button>
+			</DropdownMenuTrigger>
+
+			<DropdownMenuContent
+				className="w-56"
+				side="bottom"
+				sideOffset={8}
+				alignOffset={-8}
+				align="end"
+			>
+				<DropdownMenuGroup>
+					<DropdownMenuItem>
+						<Users className="mr-2 h-4 w-4" />
+						<span>Profil</span>
+					</DropdownMenuItem>
+					<DropdownMenuSub>
+						<DropdownMenuSubTrigger>
+							{theme === 'light' ? (
+								<Sun className="square-4 mr-2" />
+							) : (
+								<Moon className="square-4 mr-2" />
+							)}
+							<span>Toggle theme</span>
+						</DropdownMenuSubTrigger>
+						<DropdownMenuPortal>
+							<DropdownMenuSubContent>
+								<DropdownMenuItem onClick={() => setTheme('light')}>
+									<Sun className="square-4 mr-2" />
+									<span>Light</span>
+									{theme === 'light' && <Check className="square-4 ml-auto" />}
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setTheme('dark')}>
+									<Moon className="square-4 mr-2" />
+									<span>Dark</span>
+									{theme === 'dark' && <Check className="square-4 ml-auto" />}
+								</DropdownMenuItem>
+								<DropdownMenuSeparator />
+								<DropdownMenuItem onClick={() => setTheme('system')}>
+									<Settings className="square-4 mr-2" />
+									<span>System</span>
+									{theme === 'system' && <Check className="square-4 ml-auto" />}
+								</DropdownMenuItem>
+							</DropdownMenuSubContent>
+						</DropdownMenuPortal>
+					</DropdownMenuSub>
+				</DropdownMenuGroup>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem
+					onClick={() => {
+						spotify.sdk.logOut();
+						navigate({ to: '/login' });
+					}}
+				>
+					<LogOut className="mr-2 h-4 w-4" />
+					<span>Log out</span>
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
--- client/src/routes/app.tsx
+++ client/src/routes/app.tsx
@@ -1,53 +1,21 @@
 import { TypographyLarge, TypographyMuted } from '@/components/typography';
-import { Button } from '@/components/ui/button';
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuGroup,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuPortal,
-	DropdownMenuSeparator,
-	DropdownMenuShortcut,
-	DropdownMenuSub,
-	DropdownMenuSubContent,
-	DropdownMenuSubTrigger,
-	DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import {
 	Tooltip,
 	TooltipContent,
 	TooltipTrigger,
 } from '@/components/ui/tooltip';
+import { UserDropdown } from '@/components/user-dropdown';
 import { preloadQuery } from '@/lib/preload-query';
 import { useSpotifyPlayerStore } from '@/lib/providers/SpotifyPlayerProvider';
-import { useTheme } from '@/lib/providers/ThemeProvider';
 import {
 	Link,
 	Outlet,
 	createFileRoute,
 	redirect,
-	useNavigate,
 } from '@tanstack/react-router';
 import { usePreloadedQuery } from 'convex/react';
-import { Home, Moon, Music, Sun } from 'lucide-react';
+import { Home, Music } from 'lucide-react';
 import { api } from 'server';
-import {
-	Cloud,
-	CreditCard,
-	Github,
-	Keyboard,
-	LifeBuoy,
-	LogOut,
-	Mail,
-	MessageSquare,
-	Plus,
-	PlusCircle,
-	Settings,
-	User,
-	UserPlus,
-	Users,
-} from 'lucide-react';
 
 export const Route = createFileRoute('/app')({
 	async beforeLoad(opts) {
@@ -66,9 +34,6 @@ export const Route = createFileRoute('/app')({
 
 function LayoutComponent() {
 	const rooms = usePreloadedQuery(Route.useLoaderData());
-	const { setTheme } = useTheme();
-	const spotify = useSpotifyPlayerStore();
-	const navigate = useNavigate();
 
 	return (
 		<div className="grid grid-cols-[auto_1fr] gap-4 h-full p-4">
@@ -110,121 +75,7 @@ function LayoutComponent() {
 
 			<div className="relative h-full">
 				<div className="absolute top-6 right-6 space-x-4">
-					<DropdownMenu>
-						<DropdownMenuTrigger asChild>
-							<Button variant="outline" size="icon">
-								<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-								<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-								<span className="sr-only">Toggle theme</span>
-							</Button>
-						</DropdownMenuTrigger>
-
-						<DropdownMenuContent align="end">
-							<DropdownMenuItem onClick={() => setTheme('light')}>
-								Light
-							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setTheme('dark')}>
-								Dark
-							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setTheme('system')}>
-								System
-							</DropdownMenuItem>
-						</DropdownMenuContent>
-					</DropdownMenu>
-
-					<DropdownMenu>
-						<DropdownMenuTrigger asChild>
-							<Button variant="outline" size="icon">
-								<Settings className="h-[1.2rem] w-[1.2rem]" />
-							</Button>
-						</DropdownMenuTrigger>
-
-						<DropdownMenuContent className="w-56">
-							<DropdownMenuLabel>My Account</DropdownMenuLabel>
-							<DropdownMenuSeparator />
-							<DropdownMenuGroup>
-								<DropdownMenuItem>
-									<User className="mr-2 h-4 w-4" />
-									<span>Profile</span>
-									<DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-								</DropdownMenuItem>
-								<DropdownMenuItem>
-									<CreditCard className="mr-2 h-4 w-4" />
-									<span>Billing</span>
-									<DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-								</DropdownMenuItem>
-								<DropdownMenuItem>
-									<Settings className="mr-2 h-4 w-4" />
-									<span>Settings</span>
-									<DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-								</DropdownMenuItem>
-								<DropdownMenuItem>
-									<Keyboard className="mr-2 h-4 w-4" />
-									<span>Keyboard shortcuts</span>
-									<DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-								</DropdownMenuItem>
-							</DropdownMenuGroup>
-							<DropdownMenuSeparator />
-							<DropdownMenuGroup>
-								<DropdownMenuItem>
-									<Users className="mr-2 h-4 w-4" />
-									<span>Team</span>
-								</DropdownMenuItem>
-								<DropdownMenuSub>
-									<DropdownMenuSubTrigger>
-										<UserPlus className="mr-2 h-4 w-4" />
-										<span>Invite users</span>
-									</DropdownMenuSubTrigger>
-									<DropdownMenuPortal>
-										<DropdownMenuSubContent>
-											<DropdownMenuItem>
-												<Mail className="mr-2 h-4 w-4" />
-												<span>Email</span>
-											</DropdownMenuItem>
-											<DropdownMenuItem>
-												<MessageSquare className="mr-2 h-4 w-4" />
-												<span>Message</span>
-											</DropdownMenuItem>
-											<DropdownMenuSeparator />
-											<DropdownMenuItem>
-												<PlusCircle className="mr-2 h-4 w-4" />
-												<span>More...</span>
-											</DropdownMenuItem>
-										</DropdownMenuSubContent>
-									</DropdownMenuPortal>
-								</DropdownMenuSub>
-								<DropdownMenuItem>
-									<Plus className="mr-2 h-4 w-4" />
-									<span>New Team</span>
-									<DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
-								</DropdownMenuItem>
-							</DropdownMenuGroup>
-							<DropdownMenuSeparator />
-							<DropdownMenuItem>
-								<Github className="mr-2 h-4 w-4" />
-								<span>GitHub</span>
-							</DropdownMenuItem>
-							<DropdownMenuItem>
-								<LifeBuoy className="mr-2 h-4 w-4" />
-								<span>Support</span>
-							</DropdownMenuItem>
-							<DropdownMenuItem disabled>
-								<Cloud className="mr-2 h-4 w-4" />
-								<span>API</span>
-							</DropdownMenuItem>
-							<DropdownMenuSeparator />
-							<DropdownMenuItem
-								onClick={() => {
-									spotify.sdk.logOut();
-									navigate({ to: '/login' });
-								}}
-							>
-								<LogOut className="mr-2 h-4 w-4" />
-								<span>Log out</span>
-								<DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-							</DropdownMenuItem>
-						</DropdownMenuContent>
-					</DropdownMenu>
+					<UserDropdown />
 				</div>
 
 				<Outlet />
