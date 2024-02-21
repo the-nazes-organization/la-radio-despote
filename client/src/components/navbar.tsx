a80acacf864b01d2342bc0171677e1f0fb6cc414
Maxime Organi <maxime.organi@gmail.com>
2024-02-21 12:05:03 +0100

    Room navbar in its own component

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,7 +1,9 @@
 import { Link } from '@tanstack/react-router';
 import { FunctionReturnType } from 'convex/server';
-import { Headphones } from 'lucide-react';
+import { Home, Music } from 'lucide-react';
 import { api } from 'server';
+import { TypographyLarge, TypographyMuted } from './typography';
+import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
 
 interface NavbarProps {
 	rooms: FunctionReturnType<typeof api.rooms.list>;
@@ -9,32 +11,35 @@ interface NavbarProps {
 
 export const Navbar = ({ rooms }: NavbarProps) => {
 	return (
-		<nav className="">
-			<Link to="/" className="[&.active]:font-bold">
-				Home
+		<nav className="flex flex-col items-center py-4 space-y-3.5">
+			<Link to="/app">
+				<Home />
 			</Link>
 
-			{rooms.map(room => (
-				<Link
-					key={room._id}
-					to="/app/$radio"
-					params={{
-						radio: room._id,
-					}}
-				>
-					<div className="rounded">
-						<div className="w-full flex items-center gap-x-2">
-							<div className=" w-12 h-12 rounded-md bg-gradient-to-br from-sky-400 to-purple-600 flex items-center justify-center text-white">
-								<Headphones />
-							</div>
+			<hr className="w-1/2" />
 
-							<h4 className="text-xs">
-								<p>{room.name}</p>
-								<span>Number of listeners</span>
-							</h4>
-						</div>
-					</div>
-				</Link>
+			{rooms.map(room => (
+				<Tooltip key={room._id}>
+					<TooltipTrigger>
+						<Link key={room._id} to="/app/$radio" params={{ radio: room._id }}>
+							<img
+								src={room.playing.spotifyTrackData.album.images[0].url}
+								alt={room.name}
+								className="square-12 rounded min-w-12"
+							/>
+						</Link>
+					</TooltipTrigger>
+
+					<TooltipContent side="right">
+						<TypographyLarge>{room.name}</TypographyLarge>
+
+						<TypographyMuted>
+							<Music className="square-[1em] inline-block mr-1" />
+							{room.playing.spotifyTrackData.name} by{' '}
+							{room.playing.spotifyTrackData.artists[0].name}
+						</TypographyMuted>
+					</TooltipContent>
+				</Tooltip>
 			))}
 		</nav>
 	);
--- client/src/routes/app.tsx
+++ client/src/routes/app.tsx
@@ -1,20 +1,9 @@
-import { TypographyLarge, TypographyMuted } from '@/components/typography';
-import {
-	Tooltip,
-	TooltipContent,
-	TooltipTrigger,
-} from '@/components/ui/tooltip';
+import { Navbar } from '@/components/navbar';
 import { UserDropdown } from '@/components/user-dropdown';
 import { preloadQuery } from '@/lib/preload-query';
 import { useSpotifyPlayerStore } from '@/lib/providers/SpotifyPlayerProvider';
-import {
-	Link,
-	Outlet,
-	createFileRoute,
-	redirect,
-} from '@tanstack/react-router';
+import { Outlet, createFileRoute, redirect } from '@tanstack/react-router';
 import { usePreloadedQuery } from 'convex/react';
-import { Home, Music } from 'lucide-react';
 import { api } from 'server';
 
 export const Route = createFileRoute('/app')({
@@ -37,42 +26,7 @@ function LayoutComponent() {
 
 	return (
 		<div className="grid grid-cols-[auto_1fr] gap-4 h-full p-4">
-			<nav className="flex flex-col items-center py-4 space-y-3.5">
-				<Link to="/app">
-					<Home />
-				</Link>
-
-				<hr className="w-1/2" />
-
-				{rooms.map(room => (
-					<Tooltip key={room._id}>
-						<TooltipTrigger>
-							<Link
-								key={room._id}
-								to="/app/$radio"
-								params={{ radio: room._id }}
-							>
-								<img
-									src={room.playing.spotifyTrackData.album.images[0].url}
-									alt={room.name}
-									className="square-12 rounded min-w-12"
-								/>
-							</Link>
-						</TooltipTrigger>
-
-						<TooltipContent side="right">
-							<TypographyLarge>{room.name}</TypographyLarge>
-
-							<TypographyMuted>
-								<Music className="square-[1em] inline-block mr-1" />
-								{room.playing.spotifyTrackData.name} by{' '}
-								{room.playing.spotifyTrackData.artists[0].name}
-							</TypographyMuted>
-						</TooltipContent>
-					</Tooltip>
-				))}
-			</nav>
-
+			<Navbar rooms={rooms} />
 			<div className="relative h-full">
 				<div className="absolute top-6 right-6 space-x-4">
 					<UserDropdown />
