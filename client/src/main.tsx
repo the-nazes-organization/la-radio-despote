ef3a60a34249eeece36c9b5250ce54b42353c20f
Maxime Organi <maxime.organi@gmail.com>
2024-02-22 10:47:57 +0100

    remove Strict

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,5 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
+import ReactDOM from 'react-dom/client';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
@@ -20,8 +19,8 @@ const rootElement = document.getElementById('app')!;
 if (!rootElement.innerHTML) {
 	const root = ReactDOM.createRoot(rootElement);
 	root.render(
-		<StrictMode>
-			<RouterProvider router={router} />
-		</StrictMode>,
+		// <StrictMode>
+		<RouterProvider router={router} />,
+		// </StrictMode>,
 	);
 }
