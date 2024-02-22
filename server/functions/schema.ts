899881dba1596cdc84f862240fe418d993face26
Maxime Organi <maxime.organi@gmail.com>
2024-02-22 10:19:50 +0100

    adding spotifyUserProfile to user

--- server/functions/schema.ts
+++ server/functions/schema.ts
@@ -50,6 +50,32 @@ export default defineSchema(
 			username: v.string(),
 			email: v.string(),
 			password: v.string(),
+			spotifyUserProfile: v.object({
+				country: v.string(),
+				display_name: v.string(),
+				email: v.string(),
+				explicit_content: v.object({
+					filter_enabled: v.boolean(),
+					filter_locked: v.boolean(),
+				}),
+				external_urls: v.object({ spotify: v.string() }),
+				followers: v.object({
+					href: v.null(),
+					total: v.float64(),
+				}),
+				href: v.string(),
+				id: v.string(),
+				images: v.array(
+					v.object({
+						height: v.float64(),
+						url: v.string(),
+						width: v.float64(),
+					}),
+				),
+				product: v.string(),
+				type: v.string(),
+				uri: v.string(),
+			}),
 		}),
 	},
 
