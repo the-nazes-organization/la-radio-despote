8e697ec149b2eeae946b21d23692ba3f71431d28
Maxime Organi <maxime.organi@gmail.com>
2024-02-27 10:59:13 +0100

    Created add-track-button

---
+++ client/src/components/add-track-button.tsx
@@ -0,0 +1,43 @@
+import { useAuthedAction } from '@/lib/useAuthedAction';
+import { Loader2, Plus } from 'lucide-react';
+import { useState } from 'react';
+import { api } from 'server';
+import { Id } from 'server/functions/_generated/dataModel';
+import { Button } from './ui/button';
+
+interface AddTrackButtonProps {
+	spotifyTrackId: string;
+	roomId: Id<'rooms'>;
+}
+
+export const AddTrackButton = ({
+	roomId,
+	spotifyTrackId,
+}: AddTrackButtonProps) => {
+	const authedRequestTrack = useAuthedAction(api.tracksActions.requestTrack);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const handleClick = async () => {
+		try {
+			setIsLoading(true);
+			await authedRequestTrack({
+				spotifyTrackId,
+				roomId,
+			});
+		} finally {
+			setTimeout(() => {
+				setIsLoading(false);
+			}, 300);
+		}
+	};
+
+	return (
+		<Button className="rounded-full size-6" size={'icon'} onClick={handleClick}>
+			{isLoading ? (
+				<Loader2 className={' animate-spin size-4 min-w-6'} />
+			) : (
+				<Plus />
+			)}
+		</Button>
+	);
+};
