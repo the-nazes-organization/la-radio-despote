c243e06c981f1072d2bf9562ce061ed1edd85134
Maxime Organi <maxime.organi@gmail.com>
2024-02-27 16:09:33 +0100

    adding usecallback to useauthed Action

--- client/src/lib/useAuthedAction.ts
+++ client/src/lib/useAuthedAction.ts
@@ -1,10 +1,11 @@
 import { useAction } from 'convex/react';
-import { useSpotifyPlayerStore } from './providers/SpotifyPlayerProvider';
 import {
 	DefaultFunctionArgs,
 	FunctionReference,
 	OptionalRestArgs,
 } from 'convex/server';
+import { useCallback } from 'react';
+import { useSpotifyPlayerStore } from './providers/SpotifyPlayerProvider';
 
 export const useAuthedAction = <TArgs extends DefaultFunctionArgs>(
 	actionReference: FunctionReference<'action', 'public', TArgs>,
@@ -12,25 +13,28 @@ export const useAuthedAction = <TArgs extends DefaultFunctionArgs>(
 	const { sdk } = useSpotifyPlayerStore();
 	const actionFunction = useAction(actionReference);
 
-	return async (
-		arg: Omit<
-			OptionalRestArgs<FunctionReference<'action', 'public', TArgs>>[0],
-			'token'
-		>,
-	) => {
-		const token = await sdk.getAccessToken().then(auth => {
-			if (!auth) {
-				throw new Error('Not authenticated');
-			}
-			return auth.access_token;
-		});
+	return useCallback(
+		async (
+			arg: Omit<
+				OptionalRestArgs<FunctionReference<'action', 'public', TArgs>>[0],
+				'token'
+			>,
+		) => {
+			const token = await sdk.getAccessToken().then(auth => {
+				if (!auth) {
+					throw new Error('Not authenticated');
+				}
+				return auth.access_token;
+			});
 
-		const argsWithToken = { ...arg, token };
+			const argsWithToken = { ...arg, token };
 
-		actionFunction(
-			...([argsWithToken] as unknown as OptionalRestArgs<
-				FunctionReference<'action', 'public', TArgs>
-			>),
-		);
-	};
+			actionFunction(
+				...([argsWithToken] as unknown as OptionalRestArgs<
+					FunctionReference<'action', 'public', TArgs>
+				>),
+			);
+		},
+		[actionFunction, sdk],
+	);
 };
