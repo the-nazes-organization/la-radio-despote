b5fb77223191e9719f9b9a7ade1806c31357fdfe
Maxime Organi <maxime.organi@gmail.com>
2024-02-29 19:20:45 +0100

    adding tanstack QueryClient

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import { TooltipProvider } from '@/components/ui/tooltip';
+import { QueryClientProviderComponent } from '@/lib/providers/QueryClientProvider';
 import { ThemeProvider } from '@/lib/providers/ThemeProvider';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import ConvexClientProvider from '../lib/providers/ConvexProvider';
@@ -8,11 +9,13 @@ export const Route = createRootRoute({
 	component: () => {
 		return (
 			<ConvexClientProvider>
-				<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-					<TooltipProvider delayDuration={0}>
-						<Outlet />
-					</TooltipProvider>
-				</ThemeProvider>
+				<QueryClientProviderComponent>
+					<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+						<TooltipProvider delayDuration={0}>
+							<Outlet />
+						</TooltipProvider>
+					</ThemeProvider>
+				</QueryClientProviderComponent>
 			</ConvexClientProvider>
 		);
 	},
