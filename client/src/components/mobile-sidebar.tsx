e9089ba992a5d696dbd99ec3d38b859650e0587b
Maxime Organi <maxime.organi@gmail.com>
2024-02-29 19:01:55 +0100

    adding current active radio on mobile side bar

--- client/src/components/mobile-sidebar.tsx
+++ client/src/components/mobile-sidebar.tsx
@@ -1,7 +1,10 @@
+import { cn } from '@/lib/utils';
+import { Route } from '@/routes/app/$radio';
 import { Link } from '@tanstack/react-router';
 import { FunctionReturnType } from 'convex/server';
 import { Home, Menu, Music, Users } from 'lucide-react';
 import { api } from 'server';
+import { Id } from 'server/functions/_generated/dataModel';
 import { TypographyH3, TypographyMuted, TypographySmall } from './typography';
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
 
@@ -10,6 +13,7 @@ interface MobileSidebarProps {
 }
 
 export const MobileSidebar = ({ rooms }: MobileSidebarProps) => {
+	const params = Route.useParams<{ radio: Id<'rooms'> }>();
 	return (
 		<Sheet>
 			<SheetTrigger
@@ -26,35 +30,41 @@ export const MobileSidebar = ({ rooms }: MobileSidebarProps) => {
 				<hr className="my-6" />
 				<TypographyH3>Radios</TypographyH3>
 				<ul className="space-y-3 pt-4">
-					{rooms.map(room => (
-						<Link
-							key={room._id}
-							to="/app/$radio"
-							params={{ radio: room._id }}
-							className="grid grid-cols-[40px_1fr_auto] gap-4"
-						>
-							<img
-								src={room.playing.spotifyTrackData.album.images[0].url}
-								alt={room.name}
-								className="square-12 rounded min-w-12 self-center"
-							/>
-							<div className=" self-center">
-								<TypographySmall className=" line-clamp-1">
-									{room.name}
-								</TypographySmall>
+					{rooms.map(room => {
+						const isActiveRadio = room._id === params.radio;
+						return (
+							<Link
+								key={room._id}
+								to="/app/$radio"
+								params={{ radio: room._id }}
+								className={cn(
+									'grid grid-cols-[40px_1fr_auto] gap-4 p-2',
+									isActiveRadio && 'bg-secondary rounded',
+								)}
+							>
+								<img
+									src={room.playing.spotifyTrackData.album.images[0].url}
+									alt={room.name}
+									className="square-12 rounded min-w-12 self-center"
+								/>
+								<div className=" self-center">
+									<TypographySmall className=" line-clamp-1">
+										{room.name}
+									</TypographySmall>
 
-								<TypographyMuted className=" line-clamp-1">
-									<Music className="square-[1em] inline-block mr-1" />
-									{room.playing.spotifyTrackData.name} by{' '}
-									{room.playing.spotifyTrackData.artists[0].name}
-								</TypographyMuted>
-							</div>
-							<div className="flex self-center">
-								<Users />
-								{room.listeners.length}
-							</div>
-						</Link>
-					))}
+									<TypographyMuted className=" line-clamp-1">
+										<Music className="square-[1em] inline-block mr-1" />
+										{room.playing.spotifyTrackData.name} by{' '}
+										{room.playing.spotifyTrackData.artists[0].name}
+									</TypographyMuted>
+								</div>
+								<div className="flex self-center">
+									<Users />
+									{room.listeners.length}
+								</div>
+							</Link>
+						);
+					})}
 				</ul>
 			</SheetContent>
 		</Sheet>
