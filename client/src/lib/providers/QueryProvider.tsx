8e43d72ec60df84ed9e8e88b052bcfa69f052a0c
Maxime Organi <maxime.organi@gmail.com>
2024-02-29 19:23:37 +0100

    rename QueryProvider

--- client/src/lib/providers/QueryProvider.tsx
+++ client/src/lib/providers/QueryProvider.tsx
@@ -2,11 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
-export const QueryClientProviderComponent = ({
-	children,
-}: {
-	children: React.ReactNode;
-}) => {
+export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 	);
--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { QueryClientProviderComponent } from '@/lib/providers/QueryClientProvider';
+import { QueryProvider } from '@/lib/providers/QueryProvider';
 import { ThemeProvider } from '@/lib/providers/ThemeProvider';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import ConvexClientProvider from '../lib/providers/ConvexProvider';
@@ -9,13 +9,13 @@ export const Route = createRootRoute({
 	component: () => {
 		return (
 			<ConvexClientProvider>
-				<QueryClientProviderComponent>
+				<QueryProvider>
 					<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
 						<TooltipProvider delayDuration={0}>
 							<Outlet />
 						</TooltipProvider>
 					</ThemeProvider>
-				</QueryClientProviderComponent>
+				</QueryProvider>
 			</ConvexClientProvider>
 		);
 	},
